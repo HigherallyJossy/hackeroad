9a762a93847cc0011bee4528313403d2d7ca2790
Gerasimov1991 <svt.triya1991@gmail.com>
2020-03-15 04:11:22 +1000

    update

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -5,9 +5,9 @@
 
 @section('page_custom')
     <div>
-        {{-- <h1 class="error_title">Page Expired!</h1>         --}}
+        <h1 class="error_title">Page Expired!</h1>        
     </div>
     <script>
-        document.location.href="/";
+        // document.location.href="/";
     </script>
 @endsection
